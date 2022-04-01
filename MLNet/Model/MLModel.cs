669df250a8fd6a98f6b4c2dde0b033d8092af06f
xin-pu <pu.xin@outlook.com>
2022-04-01 21:50:17 +0800

    go on.

---
+++ MLNet/Model/MLModel.cs
@@ -0,0 +1,32 @@
+﻿using MathNet.Numerics.LinearAlgebra.Double;
+using System.Text;
+
+namespace MLNet.Model
+{
+    public abstract class MLModel
+    {
+        public MLModel(string name)
+        {
+            Name= name;
+        }
+
+        public string Name { get; set; }
+
+        public abstract void Fit(Matrix X, Matrix Y);
+
+        public abstract double Evaluate(Matrix X, Matrix Y);
+
+        public abstract double Predict(Matrix X, Matrix Y);
+
+        public abstract void Save(string path);
+
+        public abstract MLModel Load(string path);
+
+        public override string ToString()
+        {
+            var strBuild = new StringBuilder();
+            strBuild.AppendLine($"Name");
+            return strBuild.ToString();
+        }
+    }
+}
