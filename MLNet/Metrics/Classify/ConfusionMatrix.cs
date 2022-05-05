4721f9f4aaea4cf418f84221e179a97323aaaeed
xin-pu <pu.xin@outlook.com>
2022-05-05 17:16:35 +0800

    optimize O(∩_∩)O

--- MLNet/Metrics/Classify/ConfusionMatrix.cs
+++ MLNet/Metrics/Classify/ConfusionMatrix.cs
@@ -3,29 +3,6 @@ using Numpy;
 
 namespace MLNet.Metrics
 {
-    /// <summary>
-    ///     ConfusionMatrix
-    ///     混淆矩阵
-    ///     pred==c  |  pred!=c     |
-    ///     TP       |      FN      |   true==c
-    ///     FP       |      TN      |   true!=c
-    /// </summary>
-    public class ConfusionMatrixs
-    {
-        public ConfusionMatrixs(NDarray y_true, NDarray y_pred)
-        {
-            var classes = y_true.GetData<int>().Distinct();
-            ConfusionMatrixDict = classes
-                .ToList()
-                .OrderBy(c => c)
-                .ToDictionary(c => c, c => new ConfusionMatrix(y_true, y_pred, c));
-        }
-
-
-        public Dictionary<int, ConfusionMatrix> ConfusionMatrixDict { set; get; }
-    }
-
-
     public class ConfusionMatrix
     {
         public ConfusionMatrix(
