897fd1b72648d5d20ab807db14e679a0779ed04b
xin-pu <pu.xin@outlook.com>
2022-05-09 13:45:11 +0800

    Add Momentum Optimizer.

--- MLNet/Optimizers/Momentum.cs
+++ MLNet/Optimizers/Momentum.cs
@@ -4,7 +4,7 @@ namespace MLNet.Optimizers
 {
     public class Momentum : Optimizer
     {
-        public Momentum(double learningrate, double gamma = 0)
+        public Momentum(double learningrate, double gamma = 0.9)
             : base(learningrate)
         {
             Gamma = gamma;
@@ -12,10 +12,16 @@ namespace MLNet.Optimizers
 
         public double Gamma { set; get; }
 
+        public NDarray DeltaTheda { protected set; get; } = null!;
 
         internal override NDarray call(NDarray weight, NDarray grad, int epoch = 0)
         {
-            throw new NotImplementedException();
+            if (epoch == 0)
+                DeltaTheda = np.zeros_like(weight);
+
+            DeltaTheda = Gamma * DeltaTheda - LearningRate * grad;
+
+            return weight + DeltaTheda;
         }
     }
 }
