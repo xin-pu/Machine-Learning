78f5e7ca2cc364aabf82f779d9be3c1ffa924e9c
xin-pu <pu.xin@outlook.com>
2022-05-13 10:48:43 +0800

    Add iris.data

---
+++ Demo/LogicClassifyTask.cs
@@ -0,0 +1,28 @@
+﻿using MLNet;
+using MLNet.Losses;
+using MLNet.Metrics;
+using MLNet.Models.Regression;
+using MLNet.Optimizers;
+using Numpy;
+
+namespace Demo
+{
+    public class LogicClassifyTask
+    {
+        public static void Run()
+        {
+            var x = Enumerable.Range(0, 200).Select(a => a * 0.01 - 2).ToArray();
+            var X = np.expand_dims(np.array(x), -1);
+            var Y = -2 + 1.5 * X - 3 * np.square(X);
+
+
+            var pr = new PolyRegression(2);
+            var trainPlan = new TrainPlan(1000, learningRate: 5E-2);
+
+            pr.GiveOptimizer(new Momentum(trainPlan.LearningRate));
+            pr.GiveLoss(new LSLoss {Constraint = Constraint.None});
+            pr.GiveMetric(new MSE(), new MAE());
+            pr.Fit(X, Y, trainPlan);
+        }
+    }
+}
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,23 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using Demo;
 using MLNet;
-using MLNet.Losses;
-using MLNet.Metrics;
-using MLNet.Models.Regression;
-using MLNet.Optimizers;
-using Numpy;
 
 Log.print = Console.WriteLine;
 Console.WriteLine("Start");
-var x = Enumerable.Range(0, 200).Select(a => a * 0.01 - 2).ToArray();
-var X = np.expand_dims(np.array(x), -1);
-var Y = -2 + 1.5 * X - 3 * np.square(X);
-
-
-var pr = new PolyRegression(2);
-var trainPlan = new TrainPlan(1000, learningRate: 5E-2);
-
-pr.GiveOptimizer(new Momentum(trainPlan.LearningRate));
-pr.GiveLoss(new LSLoss {Constraint = Constraint.None});
-pr.GiveMetric(new MSE(), new MAE());
-pr.Fit(X, Y, trainPlan);
+PolyRegressionTask.Run();
