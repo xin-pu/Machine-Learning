5eb7210ae4f15b66048b73cae43f9a29d5853933
xin-pu <pu.xin@outlook.com>
2022-05-18 15:23:36 +0800

    ε=(´ο｀*)))

---
+++ MLNetTest/Func/OptionTest.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Collections.Specialized;
+using LaYumba.Functional;
+using Xunit;
+using Xunit.Abstractions;
+using static LaYumba.Functional.F;
+
+namespace MLNetTest.Func
+{
+    public class OptionTest : AbstractUnitTest
+    {
+        public OptionTest(ITestOutputHelper testOutputHelper)
+            : base(testOutputHelper)
+        {
+        }
+
+        [Fact]
+        public void TestOption()
+        {
+            var res = greet(Some("Xin"));
+            print(res);
+        }
+
+        [Fact]
+        public void TestLookUp()
+        {
+            var list = new NameValueCollection();
+
+
+            var a = new List<double> {1, 2, 3};
+        }
+
+        private string greet(Option<string> greetee)
+        {
+            return greetee.Match(
+                () => "Sorry?",
+                name => $"Hello, {name}");
+        }
+    }
+}
