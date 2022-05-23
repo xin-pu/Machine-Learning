0acdcc739b852c32266983f1cc4fa9ac41438c59
xin-pu <pu.xin@outlook.com>
2022-05-23 11:52:17 +0800

    add PCA.

---
+++ MLNet/Models/UnSuperviserModel/PCA.cs
@@ -0,0 +1,64 @@
+﻿using Numpy;
+using Numpy.Models;
+
+namespace MLNet.Models.UnSuperviserModel
+{
+    public class PCA : Model
+    {
+        public PCA(double componentRatio = 0.99)
+        {
+            ComponentRatio = componentRatio;
+        }
+
+
+        public double ComponentRatio { protected set; get; }
+
+        public void Fit(NDarray traindatas_x)
+        {
+            var mean = np.mean(traindatas_x, -1, keepdims: true);
+            var X_center = traindatas_x - mean;
+
+            var P = X_center.shape[1];
+            var cov = 1.0 / P * np.dot(X_center, X_center.T);
+
+
+            var (D, V) = np.linalg.eigh(cov);
+
+
+            var feature = D.GetData<double>();
+
+            var sum = feature.Sum() * ComponentRatio;
+
+            var dict = feature
+                .Select((v, i) => new KeyValuePair<int, double>(i, v))
+                .OrderBy(p => p.Value)
+                .ToDictionary(p => p.Key, p => p.Value);
+
+            var s = 0.0;
+            var resolve = new List<NDarray>();
+            foreach (var keyValuePair in dict)
+            {
+                s += keyValuePair.Value;
+                if (s > sum)
+                    break;
+                resolve.Add(V[$"{keyValuePair.Key}"]);
+            }
+
+            Resolve = np.stack(resolve.ToArray());
+        }
+
+
+        public override void Fit(NDarray traindatas_x, NDarray traindatas_y, TrainPlan trainPlan)
+        {
+        }
+
+        public override void InitialWeights(NDarray traindatas_x, NDarray trandatas_y)
+        {
+        }
+
+        internal override NDarray call(NDarray x, Shape shape)
+        {
+            return np.matmul(Resolve, x);
+        }
+    }
+}
---
+++ MLNetTest/PCA.cs
@@ -0,0 +1,63 @@
+﻿using Numpy;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace MLNetTest
+{
+    public class PCA : AbstractUnitTest
+    {
+        public PCA(ITestOutputHelper testOutputHelper)
+            : base(testOutputHelper)
+        {
+        }
+
+
+        /// <summary>
+        ///     Based on eigenvalue decomposition
+        /// </summary>
+        [Fact]
+        public void PCATest()
+        {
+            var X = np.array(new double[,] {{-1, -1, 0, 2, 0}, {-2, 0, 0, 1, 1}});
+            print(X);
+            var mean = np.mean(X, -1, keepdims: true);
+            var std = X - mean;
+            print(mean);
+            print(std);
+            var feature = X.shape[0];
+            var P = X.shape[1];
+            var cov = 1.0 / P * np.dot(X, X.T);
+            print(cov);
+
+            var (D, V) = np.linalg.eigh(cov);
+
+
+            print($"特征向量：\r\n{V}");
+            print($"特征值：\r\n{D}");
+
+            var c = V[$"{np.argmax(D)}"];
+            var encoded = np.matmul(V, X);
+            print($"编码\r\n:{encoded}");
+            var decoded = np.matmul(c.T, encoded);
+            print($"解码\r\n:{decoded}");
+
+            var (U, S, v) = np.linalg.svd(cov);
+            print(U);
+            print(S);
+            print(v);
+        }
+
+        /// <summary>
+        ///     Based on eigenvalue decomposition
+        /// </summary>
+        [Fact]
+        public void PCA_test()
+        {
+            var X = np.array(new double[,] {{-1, -1, 0, 2, 0}, {-2, 0, 0, 1, 1}});
+
+            var model = new MLNet.Models.UnSuperviserModel.PCA(0.5);
+            model.Fit(X);
+            print(model.Resolve);
+        }
+    }
+}
